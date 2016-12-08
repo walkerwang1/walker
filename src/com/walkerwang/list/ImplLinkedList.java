c85aaa3664e7c36725ebec6aa5cd5fbf78eb552b
walkerwang1 <walkerwang@gmail.com>
2016-12-08 13:21:30 +0800

    2016-12-8

--- src/com/walkerwang/list/ImplLinkedList.java
+++ src/com/walkerwang/list/ImplLinkedList.java
@@ -2,13 +2,32 @@ package com.walkerwang.list;
 
 import java.util.LinkedList;
 
+/**
+ * 实现LinkedList源码
+ * @author walkerwang
+ *
+ */
 public class ImplLinkedList {
 
+	/*
+	 * 1-分析LinkedList结构
+	 * 底层结构：双向链表
+	 * 
+	 * 方法：
+	 * 常用方法：add(),get(),set(),size(),remove(),peek(),poll(),node(index),
+	 * 栈和队列：peek(),poll(),element(),offer(),push(),pop(),
+	 * 双端队列：offerFirst(),offerLast(),peekFirst(),peekLast(),pollFirst(),pollLast()
+	 * 迭代器：ListItr
+	 */
+	
 	public static void main(String[] args) {
-		LinkedList<Object> linkedList = new LinkedList<>();
+		LinkedList<Object> stack = new LinkedList<>();
+		LinkedList<Object> queue = new LinkedList<>();
+		LinkedList<Object> deque = new LinkedList<>();
 		
-		linkedList.add(2);
-		linkedList.size();
+		stack.push(1);stack.push(11);stack.push(111);
+//		System.out.println(stack.poll());
+		System.out.println(stack.peek());
 	}
 	
 	@SuppressWarnings("unused")
---
+++ src/com/walkerwang/list/ImplStack.java
@@ -0,0 +1,27 @@
+package com.walkerwang.list;
+
+import java.util.Stack;
+
+/**
+ * 实现Stack
+ * @author walkerwang
+ *
+ */
+public class ImplStack {
+	
+	/*
+	 * 1-Stack　extends Vector
+	 * 底层结构：继承Vector使用数组
+	 * 
+	 * 方法：
+	 * push(),pop(),peek(),search(),empty()
+	 * 
+	 * 
+	 * 问题：
+	 * 1：Vector中元素个数为什么用elementCount表示，而不用size
+	 */
+
+	public static void main(String[] args) {
+		Stack<Object> stack = new Stack<>();
+	}
+}
